54d07055e7ec1c5550757883520911465a40ff1f
Lia Cicati <LiaCicati@yandex.com>
2023-05-16 17:29:00 +0200

    added: 2 new routes for user details and login

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 
-const { createUser } = require('../controllers/users');
+const { createUser, login } = require('../controllers/users');
 
+const usersRouter = require('./users');
+const auth = require('../middlewares/authentication');
+
+router.use('/users', auth, usersRouter);
 router.post('/signup', createUser);
+router.post('/signin', login);
 
 module.exports = router;
