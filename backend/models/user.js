8b68044eca0ab4e972e39f29fd3d0fe550e9a555
Lia Cicati <LiaCicati@yandex.com>
2023-05-16 16:27:48 +0200

    Updated user model
    Added method to find a user in the db based on their email and password credentials for login functionality

--- backend/models/user.js
+++ backend/models/user.js
@@ -20,5 +20,30 @@ const userSchema = new mongoose.Schema({
     },
 });
 
+// Find a user by their email and password
+function findUserByCredentials(email, password) {
+    // Find a user with the given email and select the password to be returned
+    return this.findOne({ email }).select('+password')
+      .then((user) => {
+        // If no user was found with the given email, show error
+        if (!user) {
+          console.log('incorrect email or password');
+          return;
+        }
+  
+        // If the user's stored password does not match the provided password, show error
+        if (user.password !== password) {
+          console.log('incorrect password');
+          return;
+        }
+  
+        // If valid credentials, return the user
+        return user;
+      });
+  }
+  
+  // Assign the findUserByCredentials function to the statics object of the userSchema
+  userSchema.statics.findUserByCredentials = findUserByCredentials;
+  
 
 module.exports = mongoose.model('user', userSchema);
