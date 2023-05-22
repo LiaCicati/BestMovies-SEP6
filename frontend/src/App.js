e264aa710864cd10d6e44ddfff13c5f555f2b118
Lia Cicati <LiaCicati@yandex.com>
2023-05-22 17:08:11 +0200

    feat: add movie to favourite list, reflect the state in the UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,45 +15,73 @@ function App() {
   const [currentUser, setCurrentUser] = useState({});
   const [movies, setMovies] = useState([]);
   const [savedMovies, setSavedMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [showMore, setShowMore] = useState(false);
+  const [displayedMovies, setDisplayedMovies] = useState([]);
 
   const navigate = useNavigate();
   const location = useLocation();
   const path = location.pathname;
 
   useEffect(() => {
-    // Check if a token exists in local storage
     const token = localStorage.getItem("token");
-    if (token) {
-      // If a token exists, make an API call to validate the token
-      userService
-        .getUser(token)
-        .then((res) => {
+
+    const checkUserToken = async () => {
+      try {
+        if (token) {
+          const res = await userService.getUser(token);
           if (res) {
-            // If the token is valid and user data is received, set isLoggedIn to true and navigate to the specified path
             setIsLoggedIn(true);
             navigate(path);
           }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    checkUserToken();
   }, []);
 
   useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    const fetchMoviesAndSavedMovies = async () => {
+      try {
+        const [userData, savedMovies] = await Promise.all([
+          userService.getUser(token),
+          userService.getFavoriteMovies(token),
+        ]);
+
+        setCurrentUser(userData);
+        setSavedMovies(savedMovies);
+        localStorage.setItem("saved-movies", JSON.stringify(savedMovies));
+        localStorage.setItem("current-user", JSON.stringify(userData));
+
+        const storedMovies = localStorage.getItem("movies");
+
+        if (storedMovies) {
+          const parsedMovies = JSON.parse(storedMovies);
+          const updatedMovies = checkSavedMovies(parsedMovies, savedMovies);
+          setMovies(updatedMovies);
+          setDisplayedMovies(updatedMovies.slice(0, getMoviesCount())); // Set displayedMovies with updatedMovies
+          setShowMore(updatedMovies.length > getMoviesCount());
+        } else {
+          const data = await movieService.getMovies();
+          const movies = data.results;
+          localStorage.setItem("movies", JSON.stringify(movies));
+          const updatedMovies = checkSavedMovies(movies, savedMovies);
+          setMovies(updatedMovies);
+          setDisplayedMovies(updatedMovies.slice(0, getMoviesCount())); // Set displayedMovies with updatedMovies
+          setShowMore(updatedMovies.length > getMoviesCount());
+        }
+      } catch (error) {
+        console.error("Error fetching movies and saved movies:", error);
+      }
+    };
+
     if (loggedIn) {
-      const token = localStorage.getItem("token");
-      Promise.all([
-        userService.getUser(token),
-        userService.getFavoriteMovies(token),
-      ])
-        .then(([userData, movies]) => {
-          setCurrentUser(userData);
-          setSavedMovies(movies);
-          localStorage.setItem("saved-movies", JSON.stringify(movies));
-          localStorage.setItem("current-user", JSON.stringify(userData));
-        })
-        .catch((err) => console.log(err));
+      fetchMoviesAndSavedMovies();
     }
   }, [loggedIn]);
 
@@ -86,94 +114,130 @@ function App() {
       });
   }
 
-  function handleSaveMovie(movie) {
-    userService
-      .likeMovie(movie)
-      .then((savedMovie) => {
-        setSavedMovies([savedMovie, ...savedMovies]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+  const handleSaveMovie = async (movie) => {
+    const isSaved = savedMovies.some((item) => item.id === movie.id);
+
+    if (!isSaved) {
+      const updatedMovie = { ...movie, isSaved: true };
+
+      try {
+        // Call the likeMovie API to save the movie
+        await userService.likeMovie(movie);
+
+        // Update the movie in the savedMovies state
+        setSavedMovies((prevSavedMovies) => [updatedMovie, ...prevSavedMovies]);
+
+        // Update the movie in the displayedMovies state as well
+        setDisplayedMovies((prevDisplayedMovies) => {
+          const updatedDisplayedMovies = prevDisplayedMovies.map((m) =>
+            m.id === movie.id ? updatedMovie : m
+          );
+          return updatedDisplayedMovies;
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   function handleCardClickButton(movie) {
-    if (!movie.isSaved && !movie._id) {
+    const isSaved = savedMovies.some((item) => item.id === movie.id);
+    if (!isSaved) {
       handleSaveMovie(movie);
       console.log(movie);
     } else {
-      console.log("deleting");
+      // Remove the movie from savedMovies and update the displayedMovies
+      const updatedSavedMovies = savedMovies.filter(
+        (item) => item.id !== movie.id
+      );
+      const updatedDisplayedMovies = displayedMovies.map((m) =>
+        m.id === movie.id ? { ...m, isSaved: false } : m
+      );
+
+      setSavedMovies(updatedSavedMovies);
+      setDisplayedMovies(updatedDisplayedMovies);
+      console.log("unliking");
+
+      // Update the savedMovies array in local storage
+      localStorage.setItem("savedMovies", JSON.stringify(updatedSavedMovies));
     }
   }
 
   const checkSavedMovies = (allMovies, savedMovies) => {
     savedMovies.forEach((savedMovie) => {
       const movie = allMovies.find((item) => item.title === savedMovie.title);
-
-      // Only set isSaved if a matching movie is found
       if (movie) {
         movie.isSaved = true;
       }
     });
-
     return allMovies;
   };
 
   useEffect(() => {
-    const storedMovies = localStorage.getItem("movies");
-    if (storedMovies) {
-      const parsedMovies = JSON.parse(storedMovies);
-      setMovies(checkSavedMovies(parsedMovies, savedMovies));
-    } else {
-      movieService
-        .getMovies()
-        .then((data) => {
-          console.log(data);
-          const movies = data.results;
-          localStorage.setItem("movies", JSON.stringify(movies));
-          setMovies(checkSavedMovies(movies, savedMovies));
-        })
-        .catch((error) => {
-          console.error("Error fetching movies:", error);
-        });
-    }
-  }, [savedMovies]);
+    const handleResize = () => {
+      setDisplayedMovies(movies.slice(0, getMoviesCount()));
+    };
 
-  // Function to fetch movies from the API
-const fetchMovies = () => {
-  movieService
-    .getMovies()
-    .then((data) => {
-      const movies = data.results;
-      localStorage.setItem("movies", JSON.stringify(movies));
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [movies]);
+
+  useEffect(() => {
+    setDisplayedMovies(movies.slice(0, getMoviesCount()));
+    setShowMore(movies.length > getMoviesCount());
+  }, [movies]);
+
+  const getMoviesCount = () => {
+    switch (true) {
+      case window.innerWidth >= 944:
+        return 9;
+      case window.innerWidth >= 570:
+        return 8;
+      default:
+        return 5;
+    }
+  };
+
+  const loadMovies = () => {
+    if (window.innerWidth >= 944) {
+      return 3;
+    }
+    return 2;
+  };
+
+  const handleSearchSubmit = async (value) => {
+    setSearchValue(value.trim());
+
+    if (value.trim() !== "") {
+      try {
+        const data = await movieService.getMovieByTitle(value);
+        const searchedMovies = data.results;
+
+        setDisplayedMovies(searchedMovies.slice(0, getMoviesCount()));
+        setShowMore(searchedMovies.length > getMoviesCount());
+
+        setMovies(checkSavedMovies(searchedMovies, savedMovies));
+      } catch (error) {
+        console.error("Error searching movies:", error);
+      }
+    } else {
+      setDisplayedMovies(movies.slice(0, getMoviesCount()));
+      setShowMore(movies.length > getMoviesCount());
+
       setMovies(checkSavedMovies(movies, savedMovies));
-    })
-    .catch((error) => {
-      console.error("Error fetching movies:", error);
-    });
-};
-
-// Function to set movies from localStorage
-const setMoviesFromLocalStorage = () => {
-  const storedMovies = localStorage.getItem("movies");
-
-  try {
-    if (storedMovies) {
-      const parsedMovies = JSON.parse(storedMovies);
-      setMovies(checkSavedMovies(parsedMovies, savedMovies));
-    } else {
-      fetchMovies();
     }
-  } catch (error) {
-    console.error("Error parsing stored movies:", error);
-    fetchMovies();
-  }
-};
+  };
 
-useEffect(() => {
-  setMoviesFromLocalStorage();
-}, [savedMovies]);
+  const handleClickMoreButton = () => {
+    const loadCount = loadMovies();
+    const nextBatch = movies.slice(
+      displayedMovies.length,
+      displayedMovies.length + loadCount
+    );
 
+    setDisplayedMovies((prevMovies) => [...prevMovies, ...nextBatch]);
+    setShowMore(displayedMovies.length + loadCount < movies.length);
+  };
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
@@ -183,11 +247,15 @@ useEffect(() => {
             path="/"
             element={
               <Movies
-                movies={movies}
+                showMore={showMore}
+                displayedMovies={displayedMovies}
+                searchValue={searchValue}
+                onSearchSubmit={handleSearchSubmit}
+                onClickMoreButton={handleClickMoreButton}
                 onCardClickButton={handleCardClickButton}
               />
             }
-          ></Route>
+          />
           <Route
             path="/movies/:id"
             element={
@@ -195,12 +263,12 @@ useEffect(() => {
                 <MovieDetails />
               </>
             }
-          ></Route>
+          />
           <Route
             path="/signup"
             element={<Register onRegister={onRegister} />}
-          ></Route>
-          <Route path="/signin" element={<Login onLogin={onLogin} />}></Route>
+          />
+          <Route path="/signin" element={<Login onLogin={onLogin} />} />
           <Route
             path="/profile"
             element={
--- frontend/src/components/Movies/Movies.js
+++ frontend/src/components/Movies/Movies.js
@@ -1,112 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import SearchForm from "../SearchForm/SearchForm";
-import movieService from "../../services/movieService";
 
-function Movies({ movies, onCardClickButton }) {
-  const [searchedMovies, setSearchedMovies] = useState([]);
-  const [isSearching, setIsSearching] = useState(false);
-  const [showMore, setShowMore] = useState(false);
-  const [displayedMovies, setDisplayedMovies] = useState([]);
-  const getMoviesCount = () => {
-    switch (true) {
-      case window.innerWidth >= 944:
-        return 12;
-      case window.innerWidth >= 570:
-        return 8;
-      default:
-        return 5;
-    }
-  };
-  const loadMovies = () => {
-    if (window.innerWidth >= 944) {
-      return 3;
-    }
-    return 2;
-  };
-
-  const checkSavedMovies = (allMovies, savedMovies) => {
-    savedMovies.forEach((savedMovie) => {
-      const movie = allMovies.find((item) => item.title === savedMovie.title);
-
-      // Only set isSaved if a matching movie is found
-      if (movie) {
-        movie.isSaved = true;
-      }
-    });
-
-    return allMovies;
-  };
-  useEffect(() => {
-    setDisplayedMovies(movies.slice(0, getMoviesCount()));
-    setShowMore(movies.length > getMoviesCount());
-  }, [movies]);
-  const handleSearchSubmit = async (value) => {
-    setIsSearching(true);
-    setShowMore(false);
-    if (value.trim() !== "") {
-      try {
-        const data = await movieService.getMovieByTitle(value);
-        const movies = data.results;
-        setSearchedMovies(movies);
-        updateSavedMovies(movies); // Update saved movies for searched movies
-      } catch (error) {
-        console.error("Error searching movies:", error);
-      }
-    } else {
-      setSearchedMovies([]);
-      setIsSearching(false);
-      updateSavedMovies(movies); // Update saved movies for popular movies
-    }
-  };
-  const handleClickMoreButton = () => {
-    const nextBatch = isSearching
-      ? searchedMovies.slice(
-          displayedMovies.length,
-          displayedMovies.length + loadMovies()
-        )
-      : movies.slice(
-          displayedMovies.length,
-          displayedMovies.length + loadMovies()
-        );
-    setDisplayedMovies((prevMovies) => [...prevMovies, ...nextBatch]);
-    setShowMore(
-      displayedMovies.length + loadMovies() <
-        (isSearching ? searchedMovies.length : movies.length)
-    );
-  };
-  useEffect(() => {
-    const handleResize = () => {
-      setTimeout(() => {
-        setDisplayedMovies(
-          (isSearching ? searchedMovies : movies).slice(0, getMoviesCount())
-        );
-        setShowMore(
-          (isSearching ? searchedMovies : movies).length > getMoviesCount()
-        );
-      }, 1000);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, [movies, searchedMovies, isSearching]);
-  // Function to update saved movies
-  const updateSavedMovies = (movies) => {
-    const updatedMovies = checkSavedMovies(movies, searchedMovies);
-    setDisplayedMovies(updatedMovies);
-    setShowMore(updatedMovies.length > getMoviesCount());
-  };
+function Movies({
+  showMore,
+  displayedMovies,
+  onSearchSubmit,
+  onClickMoreButton,
+  onCardClickButton,
+}) {
   return (
     <div className="movies">
-      {/* Search form component */}
-      <SearchForm onSearchSubmit={handleSearchSubmit} />
-      {/* Movie card list component */}
+      <SearchForm onSearchSubmit={onSearchSubmit} />
       <MoviesCardList
         cards={displayedMovies}
-        onClickMoreButton={handleClickMoreButton}
+        onClickMoreButton={onClickMoreButton}
         onCardClickButton={onCardClickButton}
         buttonMore={showMore}
       />
     </div>
   );
 }
+
 export default Movies;
