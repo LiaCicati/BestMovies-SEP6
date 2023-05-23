ad27c3d1e4345f0a031385fd8ae9e993eedff40a
Lia Cicati <LiaCicati@yandex.com>
2023-05-23 11:20:39 +0200

    feat: getMovieById method for retrieving movie details of fav movies

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -5,5 +5,6 @@ const { validateMovieId } = require("../middlewares/celebrateValidation");
 router.get("/", MovieController.getMovies);
 router.post("/", MovieController.addMovie);
 router.delete("/:movieId", validateMovieId, MovieController.deleteMovie);
+router.get("/:movieId", validateMovieId, MovieController.getMovieById);
 
 module.exports = router;
