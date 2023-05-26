1ca9c78bc016ff406a4dcea8ee8720f10379396a
Lori <loredanacicati@gmail.com>
2023-05-26 11:40:45 +0200

    backend: validate request for updating the user data

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const UserController = require('../controllers/user');
+const { validateUserUpdate } = require("../middlewares/celebrateValidation");
 
 router.get("/me", UserController.getUser);
-router.patch('/me', UserController.updateUser);
+router.patch('/me', validateUserUpdate, UserController.updateUser);
 
 module.exports = router;
