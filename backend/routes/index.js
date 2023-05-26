c3e73d2306d362702d30d3db78be7d89c54eb02c
Lia Cicati <LiaCicati@yandex.com>
2023-05-26 23:11:59 +0200

    added: routes for rating movies

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,19 @@
 const router = require("express").Router();
-const {validateUserRegister, validateUserLogin} = require('../middlewares/celebrateValidation');
+const {
+  validateUserRegister,
+  validateUserLogin,
+} = require("../middlewares/celebrateValidation");
 const UserController = require("../controllers/user");
 
 const usersRouter = require("./users");
 const moviesRouter = require("./movies");
+const ratingsRouter = require("./ratings");
 const auth = require("../middlewares/authentication");
 
 router.use("/users", auth, usersRouter);
 router.use("/movies", auth, moviesRouter);
-router.post("/signup",validateUserRegister, UserController.createUser);
+router.use("/ratings", auth, ratingsRouter);
+router.post("/signup", validateUserRegister, UserController.createUser);
 router.post("/signin", validateUserLogin, UserController.login);
 
 module.exports = router;
