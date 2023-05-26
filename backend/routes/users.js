c204b18e9006f4881f24b5efd283d95c1d9107dd
Lori <loredanacicati@gmail.com>
2023-05-26 11:32:31 +0200

    backend: Implement updateUser controller  + set the route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const UserController = require('../controllers/user');
 
 router.get("/me", UserController.getUser);
+router.patch('/me', UserController.updateUser);
 
 module.exports = router;
