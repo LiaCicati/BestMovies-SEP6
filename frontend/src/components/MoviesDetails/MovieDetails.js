6852acb7256750570a37bf29eee891a266f1fd5c
Lori <loredanacicati@gmail.com>
2023-05-27 11:59:23 +0200

    Updated MovieDetails component to handle user rating a movie
    Show the user his rating, conditional rendering based on different routes

--- frontend/src/components/MoviesDetails/MovieDetails.js
+++ frontend/src/components/MoviesDetails/MovieDetails.js
@@ -1,5 +1,6 @@
 import movieService from "../../services/movieService";
 import userService from "../../services/userService";
+import ratingService from "../../services/ratingService";
 import { useParams, useLocation } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import Header from "../Header/Header";
@@ -15,6 +16,76 @@ function MovieDetails() {
   const movieId = parseInt(id);
   const location = useLocation();
   const isFavoriteMoviesPage = location.pathname.includes("favorites");
+  const isMyRatingsPage = location.pathname.includes("my-ratings");
+  const [userRating, setUserRating] = useState(0);
+  const [ratingId, setRatingId] = useState("");
+  const [ratedMovie, setRatedMovie] = useState({});
+
+  const handleRatingChange = (value) => {
+    setUserRating(value);
+
+    // Update the rating if it already exists, otherwise add a new rating
+    if (userRating) {
+      ratingService
+        .updateRating({ id: ratingId, my_rating: value })
+        .then((updatedRating) => {
+          console.log("Rating updated:", updatedRating);
+          setRatingId(updatedRating._id);
+          localStorage.setItem(
+            `rating_${movie.id}`,
+            JSON.stringify(updatedRating)
+          );
+        })
+        .catch((error) => {
+          console.error("Error updating rating:", error);
+        });
+    } else if (ratedMovie.my_rating) {
+      ratingService
+        .updateRating({ id: ratedMovie._id, my_rating: value })
+        .then((updatedRating) => {
+          console.log("Rating updated:", updatedRating);
+          setRatingId(updatedRating._id);
+          localStorage.setItem(
+            `rating_${ratedMovie.movieId}`,
+            JSON.stringify(updatedRating)
+          );
+          setRatedMovie(updatedRating);
+        })
+        .catch((error) => {
+          console.error("Error updating rating:", error);
+        });
+    } else {
+      ratingService
+        .addRating({
+          movieId: movie.id,
+          title: movie.title,
+          backdrop_path: movie.backdrop_path,
+          poster_path: movie.poster_path,
+          my_rating: value,
+        })
+        .then((addedRating) => {
+          setRatingId(addedRating._id);
+          localStorage.setItem(
+            `rating_${movie.id}`,
+            JSON.stringify(addedRating)
+          );
+          setRatedMovie(addedRating);
+        })
+        .catch((error) => {
+          console.error("Error adding rating:", error);
+        });
+    }
+  };
+
+  useEffect(() => {
+    const storedRating = localStorage.getItem(`rating_${movie.id}`);
+    if (storedRating) {
+      const parsedRating = JSON.parse(storedRating);
+      setUserRating(parsedRating.my_rating);
+      setRatingId(parsedRating._id);
+      setRatedMovie(parsedRating);
+    }
+  }, [movie.id]);
 
   const fetchMovieDetails = async (movieId) => {
     try {
@@ -43,6 +114,16 @@ function MovieDetails() {
     }
   };
 
+  const fetchMyRatings = async (id) => {
+    try {
+      const data = await ratingService.getRatingById(id);
+      const rating = data;
+      setRatedMovie(rating);
+    } catch (error) {
+      console.error("Error fetching rated movies:", error);
+    }
+  };
+
   useEffect(() => {
     const getMovieDetails = async () => {
       const storedMovie = localStorage.getItem(`movie_${movieId}`);
@@ -63,6 +144,8 @@ function MovieDetails() {
         try {
           if (isFavoriteMoviesPage) {
             await fetchFavoriteMovieDetails(id);
+          } else if (isMyRatingsPage) {
+            await fetchMyRatings(id);
           } else {
             await fetchMovieDetails(movieId);
           }
@@ -73,81 +156,133 @@ function MovieDetails() {
     };
 
     getMovieDetails();
-  }, [movieId, id, isFavoriteMoviesPage]);
+  }, [movieId, id, isFavoriteMoviesPage, isMyRatingsPage]);
 
   if (
     Object.keys(movie).length === 0 &&
-    Object.keys(favoriteMovie).length === 0
+    Object.keys(favoriteMovie).length === 0 &&
+    Object.keys(ratedMovie).length === 0
   ) {
     return <div>Movie details not found.</div>;
   }
   return (
     <div>
       <Header />
-
-      <div className="details">
-        {isFavoriteMoviesPage ? (
-          <>
-            <div className="details__poster-container">
-              <img
-                className="details__poster"
-                src={favoriteMovie.poster_path}
-                alt={favoriteMovie.title}
-              />
+      {isFavoriteMoviesPage && (
+        <div className="details">
+          <div className="details__poster-container">
+            <img
+              className="details__poster"
+              src={favoriteMovie.poster_path}
+              alt={favoriteMovie.title}
+            />
+          </div>
+          <div>
+            <h2 className="details__header-title">{favoriteMovie.title}</h2>
+            <div className="details__container">
+              <p>{favoriteMovie.release_date}</p>
+              <p>{favoriteMovie.original_language}</p>
+              <p>{movie.popularity}</p>
+              <p>Rating: {favoriteMovie.vote_average}</p>
             </div>
-            <div>
-              <h2 className="details__header-title">{favoriteMovie.title}</h2>
-              <div className="details__container">
-                <p>{favoriteMovie.release_date}</p>
-                <p>{favoriteMovie.original_language}</p>
-                <p>{movie.popularity}</p>
-                <p>Rating: {favoriteMovie.vote_average}</p>
+            <h3>About:</h3>
+            <p>{favoriteMovie.overview}</p>
+          </div>
+        </div>
+      )}{" "}
+      {!isFavoriteMoviesPage && !isMyRatingsPage && (
+        <div className="details">
+          <div className="details__poster-container">
+            <img
+              className="details__poster"
+              src={API_IMG + movie.poster_path}
+              alt={movie.title}
+            />
+          </div>
+          <div>
+            <h2 className="details__header-title">{movie.title}</h2>
+            <div className="details__container">
+              <p>{movie.release_date}</p>
+              <p>{movie.original_language}</p>
+              <p>{movie.popularity}</p>
+              <p>Rating: {movie.vote_average}</p>
+            </div>
+
+            <p>
+              <b> Director:</b> <span>{director}</span>
+            </p>
+
+            <>
+              {" "}
+              <h3>Stars:</h3>
+              <div className="details__cast">
+                {cast.map((castMember, index) => (
+                  <React.Fragment key={castMember.name}>
+                    {index > 0 && ", "}
+                    {castMember.name}
+                  </React.Fragment>
+                ))}
               </div>
-              <h3>About:</h3>
-              <p>{favoriteMovie.overview}</p>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="details__poster-container">
-              <img
-                className="details__poster"
-                src={API_IMG + movie.poster_path}
-                alt={movie.title}
-              />
-            </div>
+            </>
+
+            <h3>About:</h3>
+            <p>{movie.overview}</p>
             <div>
-              <h2 className="details__header-title">{movie.title}</h2>
-              <div className="details__container">
-                <p>{movie.release_date}</p>
-                <p>{movie.original_language}</p>
-                <p>{movie.popularity}</p>
-                <p>Rating: {movie.vote_average}</p>
+              <h2>Rate the Movie</h2>
+              <div>
+                {Array.from({ length: 10 }, (_, index) => (
+                  <span
+                    key={index}
+                    onClick={() => handleRatingChange(index + 1)}
+                    style={{
+                      cursor: "pointer",
+                      fontSize: "30px",
+                      color: index < userRating ? "gold" : "gray",
+                    }}
+                  >
+                    ★
+                  </span>
+                ))}
               </div>
-
-              <p>
-                <b> Director:</b> <span>{director}</span>
-              </p>
-
-              <>
-                {" "}
-                <h3>Stars:</h3>
-                <div className="details__cast">
-                  {cast.map((castMember, index) => (
-                    <React.Fragment key={castMember.name}>
-                      {index > 0 && ", "}
-                      {castMember.name}
-                    </React.Fragment>
-                  ))}
-                </div>
-              </>
-
-              <h3>About:</h3>
-              <p>{movie.overview}</p>
+              <p>Your rating: {userRating}</p>
             </div>
-          </>
-        )}
-      </div>
+          </div>
+        </div>
+      )}
+      {isMyRatingsPage && (
+        <div className="details">
+          <div className="details__poster-container">
+            <img
+              className="details__poster"
+              src={ratedMovie.poster_path}
+              alt={ratedMovie.title}
+            />
+          </div>
+          <div>
+            <h2 className="details__header-title">{ratedMovie.title}</h2>
+
+            <div>
+              <h2>Update rating</h2>
+              <div>
+                {Array.from({ length: 10 }, (_, index) => (
+                  <span
+                    key={index}
+                    onClick={() => handleRatingChange(index + 1)}
+                    style={{
+                      cursor: "pointer",
+                      fontSize: "30px",
+                      color: index < ratedMovie.my_rating ? "gold" : "gray",
+                    }}
+                  >
+                    ★
+                  </span>
+                ))}
+              </div>
+              <p>My rating: {ratedMovie.my_rating}</p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
