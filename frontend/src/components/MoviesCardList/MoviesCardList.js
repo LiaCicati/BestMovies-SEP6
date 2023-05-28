f6a57a65eda2f785d8ba75e71bb3dd4d8772a1d4
Lori <loredanacicati@gmail.com>
2023-05-28 11:16:21 +0200

    Display a message for the user when no search results are found

--- frontend/src/components/MoviesCardList/MoviesCardList.js
+++ frontend/src/components/MoviesCardList/MoviesCardList.js
@@ -8,18 +8,25 @@ const MoviesCardList = ({
   onClickMoreButton,
   onCardClickButton,
 }) => {
+  const areVisibleCards = cards.length > 0;
   return (
     <section className="cards">
-      <ul className="cards__list">
-        {cards.map((card) => (
-          <MoviesCard
-            key={card.id}
-            card={card}
-            onCardClickButton={onCardClickButton}
-          />
-        ))}
-      </ul>
-
+      {!areVisibleCards && (
+        <p className="cards__message">
+          Sorry, we couldn't find any movies that match your search criteria.
+        </p>
+      )}
+      {areVisibleCards && (
+        <ul className="cards__list">
+          {cards.map((card) => (
+            <MoviesCard
+              key={card.id}
+              card={card}
+              onCardClickButton={onCardClickButton}
+            />
+          ))}
+        </ul>
+      )}
       {buttonMore && (
         <div className="cards__button-container">
           <button
